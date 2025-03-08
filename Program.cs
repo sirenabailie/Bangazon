926cdd9b4d58ae9311ff3cb47e02405fedb10aed
sirenabailie <sirenabailie@gmail.com>
2025-03-08 11:50:37 -0600

    auth/connect

--- Program.cs
+++ Program.cs
@@ -2,34 +2,43 @@ using Bangazon.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
-
+using Microsoft.EntityFrameworkCore.Query;
+using Npgsql.Internal;
+using System.Security.Cryptography;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Allows passing DateTime values without time zone data
+// allows passing datetimes without time zone data
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-// Configures the database connection using PostgreSQL and Entity Framework Core
+// allows our api endpoints to access the database through Entity Framework Core
 builder.Services.AddNpgsql<BangazonDbContext>(builder.Configuration["BangazonDbConnectionString"]);
-
 // Set the JSON serializer options
 builder.Services.Configure<JsonOptions>(options =>
 {
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
-
+// Configure CORS policy to allow frontend requests
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Apply CORS before routing
+app.UseCors();
 app.UseHttpsRedirection();
 
 //Product Endpoints//
